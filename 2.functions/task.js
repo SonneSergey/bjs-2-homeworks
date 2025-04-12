1a3bd5841433794fa67d0ab56707d6a4b31d383d
Sergey <priestsitnikovsergey@gmail.com>
2025-04-12 18:41:51 +0300

    Добавлена функция makeWork в Задаче 3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -75,18 +75,15 @@ function averageEvenElementsWorker(...arr) {
 	return sumEvenElement / countEvenElement;
 }
 
-function averageEvenElementsWorker(...arr) {
-	if (arr.length === 0) return 0;
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
 
-	let sumEvenElement = 0;
-	let countEvenElement = 0;
-
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 === 0) {
-			sumEvenElement += arr[i];
-			countEvenElement++;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const result = func(...arrOfArr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
 		}
 	}
 
-	return countEvenElement === 0 ? 0 : sumEvenElement / countEvenElement;
+	return maxWorkerResult;
 }
